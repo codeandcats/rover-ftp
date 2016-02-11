1f1f54577e12811981fc465fe2fd288c066a0978
Ben <codeandcats@gmail.com>
2016-02-11 22:20:51 +1100

    Experimenting with FTP

--- index.js
+++ index.js
@@ -62,6 +62,42 @@ cli.commandsFromFile('commands.txt').then(function() {
 		.command('fetch')
 		.action(function(command) {
 			
+		})
+		
+		.command('test')
+		.action(function(command) {
+			
+		})
+		
+		.command('connect')
+		.value('url')
+		.value('user-name')
+		.value('password')
+		.value('path')
+		.action(function(command) {
+			var PromiseFtp = require('promise-ftp');
+  			
+			var ftp = new PromiseFtp();
+			
+			var options = {
+				host: command.value('url'),
+				user: command.value('user-name'),
+				password: command.value('password')
+			};
+			
+			var path = command.value('path') || '';
+			
+			ftp
+				.connect(options)
+				.catch(err => console.error(err))
+				.then(function (serverMessage) {
+					console.log('Server message: ' + serverMessage);
+					return ftp.list('/' + path);
+				}).then(function (list) {
+					console.log('Directory listing:');
+					console.dir(list);
+					return ftp.end();
+				});
 		});
 		
 	cli.run();
